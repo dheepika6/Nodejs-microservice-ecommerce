904fcb3fcece6ead2dee17f95214b11fc87218ad
Dheepika <rdheepikaamrita@gmail.com>
2023-10-26 16:12:44 -0600

    rm dist folder

--- user/dist/database/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.db_connection = void 0;
-const connection_1 = require("./connection");
-exports.db_connection = connection_1.connection;
